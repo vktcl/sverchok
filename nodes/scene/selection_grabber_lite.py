be7a4a12030c06c8631b3bd0b7f7102d5d931fd9
zeffii <tetha.z@gmail.com>
2020-04-05 16:48:07 +0200

    was going to to do this..

--- nodes/scene/selection_grabber_lite.py
+++ nodes/scene/selection_grabber_lite.py
@@ -24,44 +24,64 @@ class SvSelectionGrabberLite(bpy.types.Node, SverchCustomTreeNode):
     bl_label = 'Selection Grabber Lite'
     bl_icon = 'MOD_MASK'
 
+    selected_mode = bpy.props.EnumProperty(
+        items=[(k, k, '', i) for i, k in enumerate(["Live", "Freeze"])], 
+        description="offers , yes what does it offer",
+        default="Live", update=updateNode
+    )
+
+    node_dict = {}
+
     def sv_init(self, context):
         self.inputs.new('SvObjectSocket', "Object")
         self.outputs.new('SvStringsSocket', "Vertex Mask")
         self.outputs.new('SvStringsSocket', "Edge Mask")
         self.outputs.new('SvStringsSocket', "Face Mask")
 
+    def draw_buttons(self, context):
+        row = layout.row()
+        row.prop(self, "selected_mode", expand=True)
+
+    def get_frozen_states(self):
+        return self.node_dict.get(hash(self), ([], [], []))
+
+    def store_states_in_dict(self):
+        self.node_dict[hash(self)] = ...
+
     def process(self):
-        objects = self.inputs['Object'].sv_get()
-        if not objects:
-            return
+        if self.selected_mode == "Live":
+            objects = self.inputs['Object'].sv_get()
+            if not objects:
+                return
 
-        include_vertex = self.outputs["Vertex Mask"].is_linked
-        include_edges = self.outputs["Edge Mask"].is_linked
-        include_faces = self.outputs["Face Mask"].is_linked
+            include_vertex = self.outputs["Vertex Mask"].is_linked
+            include_edges = self.outputs["Edge Mask"].is_linked
+            include_faces = self.outputs["Face Mask"].is_linked
 
-        face_mask, edge_mask, vertex_mask = [], [], []
-        get_selected_from = lambda data: [i.select for i in data]
+            face_mask, edge_mask, vertex_mask = [], [], []
+            get_selected_from = lambda data: [i.select for i in data]
 
-        for obj in objects:
-            mesh = obj.data
+            for obj in objects:
+                mesh = obj.data
 
-            if obj == bpy.context.edit_object:
-                bm = bmesh.from_edit_mesh(mesh)
-                face_data = bm.faces
-                edge_data = bm.edges
-                vertex_data = bm.verts
-            else:
-                face_data = mesh.polygons
-                edge_data = mesh.edges
-                vertex_data = mesh.vertices
+                if obj == bpy.context.edit_object:
+                    bm = bmesh.from_edit_mesh(mesh)
+                    face_data = bm.faces
+                    edge_data = bm.edges
+                    vertex_data = bm.verts
+                else:
+                    face_data = mesh.polygons
+                    edge_data = mesh.edges
+                    vertex_data = mesh.vertices
 
-            if include_faces:
-                face_mask.append(get_selected_from(face_data))
-            if include_edges:
-                edge_mask.append(get_selected_from(edge_data))                    
-            if include_vertex:
-                vertex_mask.append(get_selected_from(vertex_data))                    
-
+                if include_faces:
+                    face_mask.append(get_selected_from(face_data))
+                if include_edges:
+                    edge_mask.append(get_selected_from(edge_data))                    
+                if include_vertex:
+                    vertex_mask.append(get_selected_from(vertex_data))                    
+        else:
+            vertex_mask, edge_mask, face_mask = self.get_frozen_states()
 
         self.outputs['Vertex Mask'].sv_set(vertex_mask)
         self.outputs['Edge Mask'].sv_set(edge_mask)
